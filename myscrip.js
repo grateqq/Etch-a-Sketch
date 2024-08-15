0f4bf04aac1bd749d9079efbfc7bdefc28840194
granaqq <156215485+grateqq@users.noreply.github.com>
2024-08-15 20:29:56 -0300

    una grilla

--- myscrip.js
+++ myscrip.js
@@ -1,1 +1,14 @@
-console.log("hola")
+console.log("hj")
+
+let divcont = document.getElementById("conta");
+console.log(divcont)
+
+for (let i= 1; i<=16; i++) {
+  let divcelda = document.createElement("div")
+  divcelda.className = "primero";
+  divcelda.textContent= i
+  console.log(divcelda)
+  
+  divcont.appendChild(divcelda)
+}
+
