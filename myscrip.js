f6b50f5c8a48c839514b1ed7969be17975245559
granaqq <156215485+grateqq@users.noreply.github.com>
2024-08-16 11:15:45 -0300

    funcionando

--- myscrip.js
+++ myscrip.js
@@ -10,8 +10,9 @@ button.addEventListener("click", function() {
   let divcont = document.getElementById("conta");
   divcont.innerHTML = "";
 
-  numero = prompt ("numero de filas", "16")
-  numero = numero * 4
+  numero1 = prompt ("tamaño= AxA, A=", "16")
+  numero = numero1 * numero1
+  
   inicio(numero)
 });
 
@@ -23,21 +24,26 @@ divbutton.appendChild(button)
 
 // creamos un elemento en la memoria
 function inicio(numeroceldas) {
-for (let i= 1; i<=numeroceldas; i++) {
-let divcelda = document.createElement("div")
-divcelda.className = "primero"
-divcelda.textContent = i
-divcelda.id = "celda" + i
-divcelda.addEventListener("mouseover", function() {
-  // Cambiar el color de fondo cuando el mouse pasa sobre el div
-  divcelda.style.backgroundColor = "#2ecc71";
-});
-divcelda.addEventListener("mouseout", function() {
-  // Volver al color de fondo original cuando el mouse sale del div
-  divcelda.style.backgroundColor = "#3498db";
-});
-// Insertamos el elemento en DOM
-divcont.appendChild(divcelda)
+  let pxancho= 50*(numero1)
+  console.log("numero: " + numero1)
+  console.log(pxancho)
+  divcont.style.width=pxancho + "px" ;
+
+  for (let i= 1; i<=numeroceldas; i++) {
+  let divcelda = document.createElement("div")
+  divcelda.className = "primero"
+  divcelda.textContent = i
+  divcelda.id = "celda" + i
+  divcelda.addEventListener("mouseover", function() {
+    // Cambiar el color de fondo cuando el mouse pasa sobre el div
+    divcelda.style.backgroundColor = "#2ecc71";
+  });
+  divcelda.addEventListener("mouseout", function() {
+    // Volver al color de fondo original cuando el mouse sale del div
+    divcelda.style.backgroundColor = "#3498db";
+  });
+  // Insertamos el elemento en DOM
+  divcont.appendChild(divcelda)
 }
 }
 
