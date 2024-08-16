0340cccf34546797a4c6bed7fbd94f2a1ff4aba7
granaqq <156215485+grateqq@users.noreply.github.com>
2024-08-15 21:52:34 -0300

    funcionan las 16 grillas mauseover y mauseout

--- myscrip.js
+++ myscrip.js
@@ -1,21 +1,36 @@
 console.log("hj")
-
+// elebgimos un nodo y lo nombramos div nodo
 let divcont = document.getElementById("conta");
 console.log(divcont)
-
+// creamos un elemento en la memoria
+for (let i= 1; i<=16; i++) {
 let divcelda = document.createElement("div")
 divcelda.className = "primero"
-divcelda.textContent = "primero"
-divcelda.id = "celda"
-
+divcelda.textContent = i
+divcelda.id = "celda" + i
+divcelda.addEventListener("mouseover", function() {
+  // Cambiar el color de fondo cuando el mouse pasa sobre el div
+  divcelda.style.backgroundColor = "#2ecc71";
+});
+divcelda.addEventListener("mouseout", function() {
+  // Volver al color de fondo original cuando el mouse sale del div
+  divcelda.style.backgroundColor = "#3498db";
+});
+// Insertamos el elemento en DOM
 divcont.appendChild(divcelda)
-
-
+}
+/*
+//DOM 
 let celda = document.getElementById("celda")
 
+/*
 celda.addEventListener("mouseover", function() {
   // Cambiar el color de fondo cuando el mouse pasa sobre el div
   celda.style.backgroundColor = "#2ecc71";
+});
+celda.addEventListener("mouseout", function() {
+  // Volver al color de fondo original cuando el mouse sale del div
+  celda.style.backgroundColor = "#3498db";
 });
 
 // Agregar el evento mouseout
